ca4dbfd6c494f20991d775f3fd92fc96f35a71db
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-05-29 01:10:42 -0500

    added variable input option, and variable confirmation

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+// TO DO
+// Use the stored variable
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-
 
 const initialPrompt = () => { 
   rl.question(`Hi, I'm a calculator, let's do some math. Enter your equation below:
@@ -45,8 +46,40 @@ const nextCalcPrompt = () => {
 
 const calc = (answer) => {
   result = eval(answer)
+  let varParse;
 
-  console.log(`Beep Boop, here is the result: ${result}`);
+  if (answer.includes('let')) {
+    varParse = answer.split(' ')
+
+    if (varParse.length > 3) {
+      let varName = varParse[1]
+      let varVal = varParse[3]
+
+      console.log("longer than 3 " + varParse)
+
+      confirmVariable(varName, varVal);
+
+    } else {
+        varParse = varParse[1].split('=')
+
+        let varName = varParse[0]
+        let varVal = varParse[1]
+
+        console.log("less than 3 " + varParse)
+        confirmVariable(varName, varVal);
+    }
+
+  } else {
+  console.log(`Beep Boop, here is the result: ${result}`)
+  }
+
 }
 
 initialPrompt();
+
+const confirmVariable = (varName, varVal) => { 
+
+console.log(`I have your variable. It is: ${varName} = ${varVal}`)
+
+}
+
