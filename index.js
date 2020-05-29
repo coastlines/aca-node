c1a2f2a86f154aad31927f670deca64870830e7c
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-05-28 23:36:50 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(`Hi, I'm a calculator, let's do some math. Enter your equation below:
+`, (answer) => {
+  // TODO: Log the answer in a database
+  calc(answer);
+
+  rl.close();
+});
+
+const calc = (answer) => {
+  // this.answer = answer.split(' ')
+  // let n1 = Number(this.answer[0])
+  // let n2 = Number(this.answer[2])
+  // let operator = this.answer[1]
+
+  result = eval(answer);
+
+  console.log(`Beep Boop, here is the result: ${result}`);
+}
